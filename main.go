73da78ffbb21afd06aff4c8c0c268611fb0daab6
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-19 23:54:46 +0700

    Print the number of products in response

--- main.go
+++ main.go
@@ -54,7 +54,7 @@ func main() {
 	var data Response
 
 	json.Unmarshal(body,&data)
-
+    fmt.Println("Jumlah produk:", len(data.Items))
 	for _, p := range data.Items {
      fmt.Println(
         p.Name,

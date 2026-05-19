1a114d426464e83d078429476cd26753d7f11350
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-19 23:21:36 +0700

    Refactor main.go to simplify Telegram message sending
    Removed the parsePrice function and colly collector usage. Updated to send a message using the Telegram bot API.

--- main.go
+++ main.go
@@ -1,34 +1,23 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
 	"strconv"
-	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"github.com/gocolly/colly/v2"
 )
 
-
-func parsePrice(s string) int {
-
-	s = strings.ReplaceAll(s, "Rp", "")
-	s = strings.ReplaceAll(s, ".", "")
-	s = strings.ReplaceAll(s, ",", "")
-	s = strings.TrimSpace(s)
-
-	n, _ := strconv.Atoi(s)
-
-	return n
-}
-
 func main() {
+
 	botToken := os.Getenv("BOT_TOKEN")
 
 	chatIDStr := os.Getenv("CHAT_ID")
-	chatID, _ := strconv.ParseInt(chatIDStr,10,64)
+	chatID, err := strconv.ParseInt(chatIDStr, 10, 64)
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	bot, err := tgbotapi.NewBotAPI(botToken)
 
@@ -36,47 +25,13 @@ func main() {
 		log.Fatal(err)
 	}
 
-	c := colly.NewCollector()
+	msg := tgbotapi.NewMessage(chatID, "🔥 Test GitHub Action berhasil")
 
-	c.OnHTML("div.shopee-search-item-result__item", func(e *colly.HTMLElement) {
-
-		name := e.ChildText("div[data-sqe='name']")
-
-		currentPriceText := e.ChildText(".ZEgDH9")
-		oldPriceText := e.ChildText(".Y3DvsN")
-
-		currentPrice := parsePrice(currentPriceText)
-		oldPrice := parsePrice(oldPriceText)
-
-		if currentPrice == 0 || oldPrice == 0 {
-			return
-		}
-
-		discount := float64(oldPrice-currentPrice) / float64(oldPrice)
-
-		if discount >= 0.90 {
-
-			text := fmt.Sprintf(
-				"🔥 DISKON GILA\n\n%s\n\nNormal: Rp%d\nSekarang: Rp%d\nDiskon: %.2f%%",
-				name,
-				oldPrice,
-				currentPrice,
-				discount*100,
-			)
-
-			msg := tgbotapi.NewMessage(chatID, text)
-
-			bot.Send(msg)
-
-			fmt.Println(text)
-		}
-
-	})
-
-	err = c.Visit("https://shopee.co.id/flash_sale")
+	_, err = bot.Send(msg)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	log.Println("Pesan terkirim")
 }

d57dd499dcde1f8cae812611543c153e5fa07e88
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-20 00:53:42 +0700

    Refactor index.js for improved readability and updates

--- index.js
+++ index.js
@@ -2,45 +2,28 @@ const { chromium } = require("playwright");
 
 (async()=>{
 
-const browser=await chromium.launch({
+const browser = await chromium.launch({
     headless:true
 });
 
-const page=await browser.newPage();
+const page = await browser.newPage();
 
 await page.goto(
-"https://shopee.co.id/flash_sale",
-{
-waitUntil:"domcontentloaded"
-}
+    "https://www.blibli.com",
+    {
+        waitUntil:"domcontentloaded"
+    }
 );
 
-await page.waitForTimeout(3000);
-
-
-// pilih Bahasa Indonesia jika muncul
-
-const indoButton=
-page.locator(
-'text=Bahasa Indonesia'
-);
-
-if(await indoButton.count()>0){
-
-    await indoButton.click();
-
-    await page.waitForTimeout(
-        5000
-    );
-}
+await page.waitForTimeout(5000);
 
 await page.screenshot({
-path:"debug.png",
-fullPage:true
+    path:"debug.png",
+    fullPage:true
 });
 
 console.log(
-await page.title()
+    await page.title()
 );
 
 await browser.close();

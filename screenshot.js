19fb3e6b59cf1a41a3e060b56750c16c637b0e50
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-21 14:04:23 +0700

    Refactor browser connection and context setup
    Refactor screenshot.js to use connectOverCDP and streamline context creation.

--- screenshot.js
+++ screenshot.js
@@ -1,116 +1,44 @@
 const { chromium } = require("playwright");
 
-async function test(){
+(async()=>{
 
 let browser;
 
 try{
 
-console.log("Token:",
-process.env.BROWSERLESS_TOKEN?.slice(0,5));
+console.log(
+"Token:",
+process.env.BROWSERLESS_TOKEN?.slice(0,5)
+);
 
 console.log(
-"Connect browserless..."
+"Connect..."
 );
 
 browser=
-await chromium.connect(
-`wss://production-sfo.browserless.io?token=${process.env.BROWSERLESS_TOKEN}`
+await chromium.connectOverCDP(
+`https://production-sfo.browserless.io?token=${process.env.BROWSERLESS_TOKEN}`
 );
 
 console.log(
-"Connected!"
+"CONNECTED OK"
 );
 
-const context=
-await browser.newContext({
-
-userAgent:
-"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/136.0.0.0 Safari/537.36",
-
-viewport:{
-width:1366,
-height:768
-},
-
-locale:"id-ID",
-timezoneId:"Asia/Jakarta"
-});
+const contexts=
+browser.contexts();
 
 console.log(
-"Context dibuat"
+"Context:",
+contexts.length
 );
 
-const page=
-await context.newPage();
+}catch(e){
 
 console.log(
-"Page dibuat"
-);
-
-await page.addInitScript(()=>{
-
-Object.defineProperty(
-navigator,
-'webdriver',
-{
-get:()=>false
-}
-);
-
-});
-
-console.log(
-"Buka blibli..."
-);
-
-const response=
-await page.goto(
-"https://www.blibli.com/flashsale",
-{
-waitUntil:"domcontentloaded",
-timeout:120000
-}
+"ERROR:"
 );
 
 console.log(
-"Status:",
-response?.status()
-);
-
-console.log(
-"Tunggu render..."
-);
-
-await page.waitForTimeout(
-15000
-);
-
-console.log(
-"Title:",
-await page.title()
-);
-
-console.log(
-"Screenshot..."
-);
-
-await page.screenshot({
-
-path:"hasil.png",
-fullPage:true
-
-});
-
-console.log(
-"Screenshot selesai"
-);
-
-}
-catch(e){
-
-console.log(
-"ERROR:",
 e.message
 );
 
@@ -119,20 +47,16 @@ finally{
 
 if(browser){
 
-console.log(
-"Browser close"
-);
-
 await browser.close();
 
-}
+console.log(
+"Closed"
+);
 
 }
 
 }
 
-(async()=>{
-
-await test();
+process.exit(0);
 
 })();

882d5fc64a3d0fbabc667607f4b4d6b1a35cbb8c
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-21 12:45:41 +0700

    Update screenshot.js

--- screenshot.js
+++ screenshot.js
@@ -5,7 +5,7 @@ async function test(){
 const browser=
 await chromium.connect(
 
-wss://production-sfo.browserless.io?token=${process.env.BROWSERLESS_TOKEN}
+`wss://production-sfo.browserless.io?token=${process.env.BROWSERLESS_TOKEN}`
 
 );
 

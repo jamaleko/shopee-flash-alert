cd1bf90351a045c48f38f2f6af6011b0caf1bb50
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-21 16:24:23 +0700

    Update flash.js

--- flash.js
+++ flash.js
@@ -1,58 +1,40 @@
-const axios = require("axios");
+const axios=require("axios");
 
-async function cekFlashSale() {
+async function cek(){
 
-try {
+try{
 
-const res = await axios.get(
+const res=await axios.get(
 "https://www.blibli.com/backend/content/flashsale/v2/products",
 {
 headers:{
 "User-Agent":
-"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/136 Safari/537.36"
+"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 Chrome/136 Safari/537.36",
+
+"Cookie":
+`cf_clearance=9A0RJjuoeiWiudeOr2PoIMJXL9_pG0_GB3cO0bpFqlI-1779353126-1.2.1.1-Bo2NbCwe6ZymTXHLwO_YxFNen4GCKKshBdGL.xb6xJeb90EqdyJJJ7OdLMA5rsVnoDNXBRGL_fTFVshdbTbbnqHy8Tu5mZLvCww0uk8PFLdtQdd_TK.mhDndJoOEPp0WiEo107o8chPQ7i9DbJB8XXVGTYRNhyHFrqH8gBpw42ZeoMRcMfJDN_AsgIvxab8DxZZpFynR_DS2XGpBc4xin.xed1zp6d0RZ0Kbx_IvDL9jVH_fk6VaTvUjVfiQkP_myJOkmZSg1UwE.m6W4A3.tgCINswox5AAHv5DkadNLgAfsWH8te7xjPQI5J8NL.vVbbcHA36RqPJiYOCYLRSsVe3HxQYCTJdhSyZ.0pvFqIhZMRUZWHY_70CmnI9JUlqLHb4OkS9CByCW6j_oay01XfMzf0tTIL4GObHVBSnpMRM`
 }
 }
 );
 
-const products = res.data.data;
-
 console.log(
-"Jumlah produk:",
-products.length
-);
-
-for(const p of products.slice(0,5)){
-
-console.log(
-"Nama:",
-p.name
+"Jumlah:",
+res.data.data.length
 );
 
 console.log(
-"Harga:",
-p.offer
+res.data.data[0]
 );
-
-console.log(
-"Diskon:",
-p.discountPercentage+"%"
-);
-
-console.log(
-"=========="
-);
-
-}
 
 }catch(e){
 
 console.log(
 "ERROR:",
-e.message
+e.response?.status || e.message
 );
 
 }
 
 }
 
-cekFlashSale();
+cek();

ada3bc36ac6e277024587bb4896652e769444572
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-26 18:28:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,105 +1,41 @@
-const { chromium } = require("playwright");
+const axios=require("axios");
 
-async function run(){
-
-let browser;
+async function test(){
 
 try{
 
-console.log(
-"Connect browserless..."
-);
+const res=
+await axios.post(
 
-browser=
-await chromium.connectOverCDP(
-`wss://production-sfo.browserless.io?token=${process.env.BROWSERLESS_TOKEN}`
-);
+"https://shopee.co.id/api/v4/flash_sale/flash_sale_batch_get_items",
 
-console.log(
-"Connected"
-);
+{
 
-const context=
-browser.contexts()[0] ||
-await browser.newContext({
+promotionid:268083365429251,
 
-userAgent:
-"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36",
+itemids:[
+43867563107,
+19850207725,
+40328681658,
+24633769824
+]
 
-locale:"id-ID"
+},
 
-});
+{
 
-
-// ambil cookie dari ENV
-if(
-process.env.SHOPEE_COOKIE
-){
-
-const cookies=
-process.env.SHOPEE_COOKIE
-.split(";")
-.map(x=>{
-
-const parts=
-x.trim().split("=");
-
-const name=
-parts.shift();
-
-const value=
-parts.join("=");
-
-return{
-
-name,
-value,
-
-domain:
-".shopee.co.id",
-
-path:
-"/"
-
-};
-
-});
-
-await context.addCookies(
-cookies
-);
-
-console.log(
-"Cookie loaded:",
-cookies.length
-);
-
-}
-
-const page=
-await context.newPage();
-
-await page.setExtraHTTPHeaders({
+headers:{
 
 "accept":"*/*",
 
-"accept-language":
-"en-GB,en-US;q=0.9,en;q=0.8",
+"user-agent":
+"Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36",
 
 "referer":
-"https://shopee.co.id/flash_sale?categoryId=0&promotionId=268078273540098",
+"https://shopee.co.id/flash_sale",
 
-"x-api-source":
-"rweb",
-
-"x-requested-with":
-"XMLHttpRequest",
-
-"x-shopee-language":
-"id",
-
-"x-sz-sdk-version":
-"1.12.39",
+"cookie":
+process.env.SHOPEE_COOKIE,
 
 "af-ac-enc-dat":
 process.env.AF_DATA,
@@ -107,96 +43,39 @@ process.env.AF_DATA,
 "af-ac-enc-sz-token":
 process.env.AF_TOKEN,
 
-"sec-ch-ua":
-`"Not_A Brand";v="99","Google Chrome";v="109","Chromium";v="109"`,
+"x-requested-with":
+"XMLHttpRequest",
 
-"sec-ch-ua-mobile":
-"?1",
+"x-api-source":
+"rweb"
 
-"sec-ch-ua-platform":
-"Android"
+}
 
-});
+}
 
-page.on(
-"response",
-async(res)=>{
-
-try{
-
-const url=
-res.url();
-
-if(
-url.includes(
-"get_all_itemids"
-)
-){
-
-console.log(
-"\nAPI:",
-res.status()
 );
-
-const data=
-await res.json();
 
 console.log(
 JSON.stringify(
-data,
+res.data,
 null,
 2
 )
 );
 
-}
-
-}catch(e){}
-
-}
-);
-
-console.log(
-"Buka Shopee..."
-);
-
-await page.goto(
-"https://shopee.co.id/flash_sale?categoryId=0&promotionId=268078273540098",
-{
-waitUntil:
-"domcontentloaded",
-timeout:
-60000
-}
-);
-
-await page.waitForTimeout(
-15000
-);
-
-}
-catch(e){
+}catch(e){
 
 console.log(
 "ERROR:",
-e.message
+e.response?.status
 );
-
-}
-finally{
-
-if(browser){
-
-await browser.close();
-
-}
 
 console.log(
-"Closed"
+e.response?.data
 );
 
 }
 
 }
 
-run();
+test();

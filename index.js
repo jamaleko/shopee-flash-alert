57f5ccc35da5d8ea60a74d6f4b138943ae489444
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-26 19:45:54 +0700

    Update index.js

--- index.js
+++ index.js
@@ -18,17 +18,20 @@ exposure_items:"",
 filters:"{\"rpc_ProductId\":\"\",\"category_id\":null,\"rpc_page_size\":20}"
 },
 
-query:
-"query ComponentInfoQuery($identifier:String!,$componentId:String!,$current_session_id:String,$cursor:Int,$device:String,$exposure_items:String,$filters:String){componentInfo(identifier:$identifier,componentId:$componentId,current_session_id:$current_session_id,cursor:$cursor,device:$device,exposure_items:$exposure_items,filters:$filters){data}}"
+query:"query ComponentInfoQuery($identifier:String!,$componentId:String!,$current_session_id:String,$cursor:Int,$device:String,$exposure_items:String,$filters:String){componentInfo(identifier:$identifier,componentId:$componentId,current_session_id:$current_session_id,cursor:$cursor,device:$device,exposure_items:$exposure_items,filters:$filters){data}}"
 
 }];
 
 
-const res=await axios.post(
-"https://gql.tokopedia.com/graphql/ComponentInfoQuery",
-payload,
-{
-timeout:30000,
+const res=await axios({
+
+method:"post",
+
+url:"https://gql.tokopedia.com/graphql/ComponentInfoQuery",
+
+timeout:60000,
+
+data:payload,
 
 headers:{
 
@@ -46,11 +49,29 @@ headers:{
 "origin":
 "https://www.tokopedia.com",
 
+"accept-language":
+"en-GB,en-US;q=0.9,en;q=0.8",
+
 "bd-device-id":
 "0311116359114134912",
 
+"x-source":
+"tokopedia-lite",
+
+"x-device":
+"mobile",
+
+"x-user-id":
+"0",
+
+"x-tkpd-userid":
+"0",
+
+"x-tkpd-sessionid":
+Date.now().toString(),
+
 "sec-ch-ua":
-'"Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"',
+'"Not_A Brand";v="99","Google Chrome";v="109","Chromium";v="109"',
 
 "sec-ch-ua-mobile":
 "?1",
@@ -60,9 +81,8 @@ headers:{
 
 }
 
-}
+});
 
-);
 
 console.log(
 "STATUS:",
@@ -87,6 +107,18 @@ e.code
 console.log(
 e.message
 );
+
+if(e.response){
+
+console.log(
+JSON.stringify(
+e.response.data,
+null,
+2
+)
+);
+
+}
 
 }
 

cb7122b839f0ad0546e70425542d5393f6f86842
jamaleko <60812791+jamaleko@users.noreply.github.com>
2026-05-26 14:57:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
 const axios = require("axios");
 
-async function getFlash() {
-  try {
+const headers = {
+  "accept":"*/*",
 
+  "referer":
+  "https://shopee.co.id/flash_sale?categoryId=0&promotionId=268078273540098",
+
+  "user-agent":
+  "Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Mobile Safari/537.36",
+
+  "cookie":
+  process.env.SHOPEE_COOKIE,
+
+  "af-ac-enc-dat":
+  process.env.AF_DATA,
+
+  "af-ac-enc-sz-token":
+  process.env.AF_TOKEN
+};
+
+async function getFlash() {
+
+  try{
+
+    // ambil item id flash sale
     const res = await axios.get(
       "https://shopee.co.id/api/v4/flash_sale/get_all_itemids",
       {
-        params: {
-          need_personalize: true,
-          promotionid: "268078273540098",
-          sort_soldout: true,
-          tracker_info_version: 1
+        params:{
+          need_personalize:true,
+          promotionid:"268078273540098",
+          sort_soldout:true,
+          tracker_info_version:1
         },
-
-        headers: {
-          "accept":"*/*",
-          "referer":
-          "https://shopee.co.id/flash_sale?categoryId=0&promotionId=268078273540098",
-
-          "user-agent":
-          process.env.UA,
-
-          "cookie":
-          process.env.SHOPEE_COOKIE,
-
-          "af-ac-enc-dat":
-          process.env.AF_DATA,
-
-          "af-ac-enc-sz-token":
-          process.env.AF_TOKEN
-        }
+        headers
       }
     );
 
@@ -38,15 +42,48 @@ async function getFlash() {
       res.status
     );
 
+    const items =
+    res.data.data.item_brief_list;
+
+    console.log(
+      "Jumlah item:",
+      items.length
+    );
+
+    const ids =
+    items
+    .slice(0,20)
+    .map(x=>x.itemid);
+
+    console.log(
+      "Item IDs:"
+    );
+
+    console.log(ids);
+
+
+    // ambil detail item
+    const detail =
+    await axios.get(
+      "https://shopee.co.id/api/v4/flash_sale/flash_sale_batch_get_items",
+      {
+        params:{
+          itemids:ids.join(","),
+          promotionid:"268078273540098"
+        },
+        headers
+      }
+    );
+
     console.log(
       JSON.stringify(
-        res.data,
+        detail.data,
         null,
         2
       )
     );
 
-  } catch(e){
+  }catch(e){
 
     console.log(
       "ERROR:",
@@ -54,9 +91,12 @@ async function getFlash() {
     );
 
     console.log(
-      e.response?.data
+      e.response?.data ||
+      e.message
     );
+
   }
+
 }
 
 getFlash();
